0b18a610a51e55d8b989865d9468f0ec42d95826
Rishabh Rudani <rishabhnrudani@gmail.com.com>
2026-05-16 18:44:35 +0200

    use explicit odyseeus_rover_v2 image in SpaceRoverProject

--- sections/SpaceRoverProject.tsx
+++ sections/SpaceRoverProject.tsx
@@ -40,7 +40,7 @@ export default function SpaceRoverProject() {
             >
               <div className="relative">
                 <img
-                  src="/rover.png?v=3"
+                  src="/odyseeus_rover_v2.png?v=1"
                   alt="Odysseus Rover"
                   className="w-full h-auto object-cover rounded-xl max-h-96"
                 />
