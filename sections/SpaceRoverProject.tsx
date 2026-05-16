49572a5a8394ad590746309950830b50faa33c46
Rishabh Rudani <rishabhnrudani@gmail.com.com>
2026-05-16 18:51:11 +0200

    updated image

--- sections/SpaceRoverProject.tsx
+++ sections/SpaceRoverProject.tsx
@@ -40,7 +40,7 @@ export default function SpaceRoverProject() {
             >
               <div className="relative">
                 <img
-                  src="/odyseeus_rover_v2.png?v=1"
+                  src="/odysseus_rover.png?v=1"
                   alt="Odysseus Rover"
                   className="w-full h-auto object-cover rounded-xl max-h-96"
                 />
